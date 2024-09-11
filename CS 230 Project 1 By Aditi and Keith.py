414ef78a15bfc513dbfe085152d38cf6a480e0ad
Aditi <jhaaditi269@gmail.com>
2024-09-11 12:35:47 -0400

    Added code to check if the url is valid or not

--- CS 230 Project 1 By Aditi and Keith.py
+++ CS 230 Project 1 By Aditi and Keith.py
@@ -1,6 +1,8 @@
+'''Aditi: Added all base code on 9/9/24'''
 import json
 import random
 import string
+import validators
 
 def generate_short_id():
     """Generating a random string consisting of 3 digits followed by 3 lowercase letters."""
@@ -8,11 +10,21 @@ def generate_short_id():
     letters = ''.join(random.choices(string.ascii_lowercase, k=3))
     return numbers + letters
 
+"""Added code that checks if url already exists"""
+"""Keith Young 9/10/24"""
+'''Added code to make sure that the url entered by the user is a valid url - Aditi on 9/11/24'''
 def shorten_url(full_url):
     """Shortening the URL without validation."""
+    """Long id is mapped to short id. Ex: short_id [long_id]"""
+    if not validators.url(full_url):
+        return "Invalid URL"
     short_id = generate_short_id()
+    while short_id in url_map:
+        short_id = generate_short_id()
     url_map[short_id] = full_url
     return f"https://myApp.com/{short_id}"
+
+
 
 def get_full_url(short_id):
     """Retrieving the full URL from a shortened ID."""
@@ -42,7 +54,7 @@ while True:
         break
     elif action == 's':
         url = input("Enter a URL to shorten: ")
-        print("Shortened URL:", shorten_url(url))
+        print("Shortened URL:", (shorten_url(url)))
     elif action == 'r':
         short_id = input("Enter the short ID to retrieve URL: ").split('/')[-1]  # Just in case the full short URL is pasted
         print("Original URL:", get_full_url(short_id))
