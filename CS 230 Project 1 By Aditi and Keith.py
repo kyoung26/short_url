ce5e7be547acbc23e59689e45a040f569f8e43b0
Keith Young <keithy822@gmail.com>
2024-09-12 22:43:14 -0400

    added logs and checks if url starts with https://

--- CS 230 Project 1 By Aditi and Keith.py
+++ CS 230 Project 1 By Aditi and Keith.py
@@ -1,6 +1,23 @@
 import json
 import random
 import string
+import logging
+
+
+# Logging file
+
+logging.basicConfig(
+    filename="app.log",
+    encoding= "utf-8",
+    filemode= "a",
+    format="{asctime} - {levelname} - {message}",
+    style= "{",
+    datefmt = "%Y -%m-%d %H:%M",
+)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
 
 def generate_short_id():
     """Generating a random string consisting of 3 digits followed by 3 lowercase letters."""
@@ -19,21 +36,25 @@ def shorten_url(full_url):
     return f"https://myApp.com/{short_id}"
 
 
-
 def get_full_url(short_id):
     """Retrieving the full URL from a shortened ID."""
     return url_map.get(short_id, "Shortened URL does not exist.")
 
+
+
 def save_data():
     """Saving the dictionary to a JSON file."""
+    """Message of saved filed gets sent to log file"""
     with open('urls.json', 'w') as file:
         json.dump(url_map, file)
-
+    logger.info("Saved file")
+    
 def load_data():
     """Loading the dictionary from a JSON file."""
     try:
         with open('urls.json', 'r') as file:
             return json.load(file)
+    
     except FileNotFoundError:
         return {}
 
@@ -48,9 +69,19 @@ while True:
         break
     elif action == 's':
         url = input("Enter a URL to shorten: ")
-        print("Shortened URL:", (shorten_url(url)))
+        if url.startswith("https://"):
+            print("Shortened URL:", shorten_url(url))
+        else:
+            print("Can't do it!!!")
+        logging.info("Got a short url!!!") # Sends "Got a short url!!!" to log file
     elif action == 'r':
         short_id = input("Enter the short ID to retrieve URL: ").split('/')[-1]  # Just in case the full short URL is pasted
         print("Original URL:", get_full_url(short_id))
+        logging.info("Got back full url!!!") # Sends "Got back full url!!!" to log file
     else:
         print("Invalid action.")
+        
+        
+
+
+        
